791bf173bd95e689d5dbaa6db213be3984fc317f
Cruebee <60672128+Cruebee@users.noreply.github.com>
2020-03-23 11:28:56 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# movie_api-my-flix
+ This will be used to create an app where users can view different movies, actors, directors, and genres. They will be able to log in, update personal info, and create a list of their favorite movies.
---
+++ server.js
@@ -0,0 +1,30 @@
+const http = require("http"),
+  url = require("url"),
+  fs = require("fs");
+
+http
+  .createServer((request, response) => {
+    var addr = request.url,
+      q = url.parse(addr, true),
+      filePath = "";
+    if (q.pathname.includes("documentation")) {
+      filePath = __dirname + "/documentation.html";
+    } else {
+      filePath = "index.html";
+    }
+
+    fs.appendFile(
+      "log.txt",
+      "URL: " + addr + "\nTimestamp: " + new Date() + "\n\n",
+      function(err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Sucessfully added to log!");
+        }
+      }
+    );
+  })
+  .listen(8080);
+
+console.log("My first Node test server is running on Port 8080.");
