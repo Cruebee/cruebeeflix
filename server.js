cbd85db864cff032c5158ee7231c0710ca5072a9
Cruebee <60672128+Cruebee@users.noreply.github.com>
2020-03-23 11:45:55 -0600

    fix order of server.js page

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const http = require("http"),
-  url = require("url"),
-  fs = require("fs");
+  fs = require("fs"),
+  url = require("url");
 
 http
   .createServer((request, response) => {
     var addr = request.url,
       q = url.parse(addr, true),
       filePath = "";
+
     if (q.pathname.includes("documentation")) {
-      filePath = __dirname + "/documentation.html";
+      filePath = __dirname + "documentation.html";
     } else {
       filePath = "index.html";
     }
@@ -20,11 +21,9 @@ http
         if (err) {
           console.log(err);
         } else {
-          console.log("Sucessfully added to log!");
+          console.log("Added to log!");
         }
       }
     );
   })
   .listen(8080);
-
-console.log("My first Node test server is running on Port 8080.");
