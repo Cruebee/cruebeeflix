42ae01e22e3dfae55f55021d2c3f24c59e7fe39e
Cruebee <60672128+Cruebee@users.noreply.github.com>
2020-04-14 14:48:20 -0600

    implement the use of bcrypt for hashing passwords

--- models.js
+++ models.js
@@ -1,5 +1,7 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'),
+  bcrypt = require('bcrypt');
 
+// Movies Schema
 var movieSchema = mongoose.Schema({
   Title: {type: String, required: true},
   Description: {type: String, required: true},
@@ -9,6 +11,7 @@ var movieSchema = mongoose.Schema({
   Featured: Boolean
 });
 
+// Directors Schema
 var directorSchema = mongoose.Schema({
   Name: {type: String, required: true},
   Bio: {type: String},
@@ -16,18 +19,28 @@ var directorSchema = mongoose.Schema({
   Death: String
 });
 
+// Genres Schema
 var genreSchema = mongoose.Schema({
   Name: {type: String, required: true},
   Description: {type: String, required: true}
 });
 
+// Users Schema
 var userSchema = mongoose.Schema({
-  Username: {type: String, required: true},
-  Password: {type: String, required: true},
-  Email: {type: String, required: true},
-  Birthday: Date,
-  FavoriteMovies: [{ type : mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
+  Username : {type: String, required: true},
+  Password : {type: String, required: true},
+  Email : {type: String, required: true},
+  Birthday : Date,
+  FavoriteMovies : [{type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
 });
+
+userSchema.statistic.hashPassword = function(password) {
+  return bcrypt.hashSync(password, 10);
+};
+
+userSchema.methods.validatePassword = function(password) {
+  return bcrypt.compareSync(password, this.Password);
+};
 
 var Movie = mongoose.model('Movie', movieSchema);
 var Director = mongoose.model('Director', directorSchema);
