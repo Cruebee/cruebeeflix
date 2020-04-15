7847d0a895ed62a60d2b5e039b117bf7f4eccfd5
Cruebee <60672128+Cruebee@users.noreply.github.com>
2020-04-15 15:23:30 -0600

    connect app with db and enable heroku environment variables

--- index.js
+++ index.js
@@ -16,7 +16,9 @@ Genres = Models.Genre;
 const { check, validationResult } = require('express-validator');
 
 
-mongoose.connect('mongodb://localhost:27017/myFlixDB', {useNewUrlParser: true, useUnifiedTopology: true});
+/*mongoose.connect('mongodb://localhost:27017/myFlixDB', {useNewUrlParser: true, useUnifiedTopology: true}); */
+
+mongoose.connect('process.env.CONNECTION_URI', {useNewUrlParser: true, useUnifiedTopology: true});
 
 var allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
 
