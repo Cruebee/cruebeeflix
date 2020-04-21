1a528f102539dfc114d1e24a9ccfeb14598acc98
Cruebee <60672128+Cruebee@users.noreply.github.com>
2020-04-21 11:32:51 -0600

    add all React files to heroku

--- client/src/components/main-view/main-view.jsx
+++ client/src/components/main-view/main-view.jsx
@@ -1,13 +1,32 @@
 import React from "react";
 import axios from "axios";
 
-class MainView extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {};
+export class MainView extends React.Component {
+  componentDidMount() {
+    axios
+      .get("<my-api-endpoint/movies>")
+      .then((response) => {
+        this.setState({
+          movies: response.data,
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
+
   render() {
-    return <div className="main-view"></div>;
+    const { movies } = this.state;
+    if (!movies) return <div className="main-view" />;
+
+    return (
+      <div className="main-view">
+        {movies.map((movie) => (
+          <div className="movie-card" key={movie._id}>
+            {movie.Title}
+          </div>
+        ))}
+      </div>
+    );
   }
 }
