b4965ce33a631e19a47b0c0e3e557bb5f454c247
Cruebee <60672128+Cruebee@users.noreply.github.com>
2020-04-27 08:56:09 -0600

    Install React Bootstrap and link Bootstrap to index.html file.

--- client/src/components/main-view/main-view.jsx
+++ client/src/components/main-view/main-view.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import axios from 'axios';
+import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
 export class MainView extends React.Component {
+
   constructor() {
     super();
 
     this.state = {
       movies: null,
       selectedMovie: null,
+      user: null
     };
   }
 
@@ -33,25 +36,28 @@ export class MainView extends React.Component {
     });
   }
 
+  onLoggedIn(user) {
+    this.setState({
+      user
+    });
+  }
+
   render() {
-    const { movies, selectedMovie } = this.state;
+    const { movies, selectedMovie, user } = this.state;
+
+    if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
 
     // Before the movies have been loaded
     if (!movies) return <div className='main-view' />;
 
     return (
-      <div className='main-view'>
-        {selectedMovie ? (
-          <MovieView movie={selectedMovie} />
-        ) : (
-            movies.map((movie) => (
-              <MovieCard
-                key={movie._id}
-                movie={movie}
-                onClick={(movie) => this.onMovieClick(movie)}
-              />
-            ))
-          )}
+      <div className="main-view">
+        {selectedMovie
+          ? <MovieView movie={selectedMovie} />
+          : movies.map(movie => (
+            <MovieCard key={movie._id} movie={movie} onClick={movie => this.onMovieClick(movie)} />
+          ))
+        }
       </div>
     );
   }
