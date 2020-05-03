a0144b68234e363aaa3fc1cf76c2c1f82f19fa32
Cruebee <60672128+Cruebee@users.noreply.github.com>
2020-05-03 16:40:57 -0600

    reinstall dependencies as they broke for some reason

--- client/src/components/main-view/main-view.jsx
+++ client/src/components/main-view/main-view.jsx
@@ -4,7 +4,7 @@ import Row from 'react-bootstrap/Row';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
-import Col from 'react-bootstrat/Col';
+
 
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Link } from "react-router-dom";
