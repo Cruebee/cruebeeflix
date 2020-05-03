4344ec5fd7a5463684df4619a9efba511ee6fe43
Cruebee <60672128+Cruebee@users.noreply.github.com>
2020-05-03 10:30:15 -0600

    Add custom photos to public folder, move index.html from public folder

--- client/dist/src.78399e21.js
+++ client/dist/src.78399e21.js
@@ -35634,7 +35634,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51308" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61037" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
