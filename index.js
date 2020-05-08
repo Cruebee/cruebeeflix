a2b8dc7b15800707f545d47578d09d12074bbf47
Cruebee <60672128+Cruebee@users.noreply.github.com>
2020-05-08 11:34:56 -0600

    fix formatting inconsistencies

--- index.js
+++ index.js
@@ -42,9 +42,9 @@ app.use(morgan('common'));
 // use express.static to return all static files within 'public' folder
 app.use(express.static('public'));
 // implement "/client" directory (linking app to host on Heroku)
-app.use("/client", express.static(path.join(__dirname, "client", "dist")));
-app.get("/client/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+app.use('/client', express.static(path.join(__dirname, 'client', 'dist')));
+app.get('/client/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 });
 
 // initialize the body-parser module
