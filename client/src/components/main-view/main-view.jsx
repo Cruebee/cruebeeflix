aca5138ceb292ab0dca93a07fea193e3184eb8ee
Cruebee <60672128+Cruebee@users.noreply.github.com>
2020-05-08 10:33:27 -0600

    add basename=/client to router to add /client to all client-side requests

--- client/src/components/main-view/main-view.jsx
+++ client/src/components/main-view/main-view.jsx
@@ -92,7 +92,7 @@ class MainView extends React.Component {
     let { user } = this.state;
 
     return (
-      <Router>
+      <Router basename="/client">
         <Container className="main-view" fluid="true">
           <Navbar className="navbar navbar-dark">
             <h1 className="main-view-title">myFlix</h1>
